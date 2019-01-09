7e13147f3e1a6d8355f4bf4080af0ad797bb8a9c
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-09 23:32:41 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,21 +1,9 @@
 class Author
-  attr_accessor :name, :songs
+  attr_accessor :name, 
 
   def initialize(name)
     @name = name
   end
 
-  def songs
-    songs = []
-  end
-
-  def add_song(song)
-    songs << @song
-  end
-
-  def add_song_by_name
-  end
-
-  def song_count
-  end
+=
 end
