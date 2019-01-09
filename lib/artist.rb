f9fc632e22c5f4f607324ad54af90528dcc841be
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-09 23:32:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,4 +4,18 @@ class Artist
   def initialize(name)
     @name = name
   end
+
+  def songs
+    songs = []
+  end
+
+  def add_song(song)
+    songs << @song
+  end
+
+  def add_song_by_name
+  end
+
+  def song_count
+  end
 end
