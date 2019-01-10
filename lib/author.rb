507c13fc8d6244a608933e540355fadf6c31e558
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-10 00:34:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,16 +12,16 @@ end
 #   def songs
 #     Song.all.select {|song| song.artist == self}
 #   end
-# 
+#
 #   def add_song(song)
 #     song.artist = self
 #   end
-# 
+#
 #   def add_song_by_name(name)
 #     song = Song.new(name)
 #     song.artist = self
 #   end
-# 
+#
 #   def self.song_count
 #     Song.all.count
 #   end
