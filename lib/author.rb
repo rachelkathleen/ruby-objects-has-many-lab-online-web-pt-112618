1ec48f009c13324b2b4dd44b4a28b5b1caa8adc9
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-10 00:37:28 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,19 +15,13 @@ class Author
     post.author = self
   end
 
-  def add_post_by_title
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
   end
 end
 
 
-#   def songs
-#     Song.all.select {|song| song.artist == self}
-#   end
-#
-#   def add_song(song)
-#     song.artist = self
-#   end
-#
 #   def add_song_by_name(name)
 #     song = Song.new(name)
 #     song.artist = self
